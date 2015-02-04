3e3973d47b4e9fac8313ca38e67bb40c9fcded15
Will Barton <william.barton@cfpb.gov>
2015-02-04 09:26:43 -0500

    Remove namespace collision between local random module and python random module.

--- porchlightapi/sources/__init__.py
+++ porchlightapi/sources/__init__.py
@@ -3,5 +3,5 @@
 from .calculators import difference_value_calculator
 from .calculators import undeployed_value_only_calculator
 
-from .random import random_source
+from .rand import random_source
 from .github import github_source

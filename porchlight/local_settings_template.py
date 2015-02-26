27897de3cba2c42fba0dc6d4ecdb2ab926598f38
Will Barton <william.barton@cfpb.gov>
2015-02-26 14:35:54 -0500

    Move the secret key to local_settings_template.py

--- porchlight/local_settings_template.py
+++ porchlight/local_settings_template.py
@@ -1,5 +1,7 @@
 
 
+# Secret key should live in your local settings.py and should be unique.
+SECRET_KEY = 'sssh-make-this-unique'
 
 # Set this if you want to load a JSON file from a specific path.
 PORCHLIGHT_JSON_FILE = ''
